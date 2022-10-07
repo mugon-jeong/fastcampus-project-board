978f4c00e12c9c33cabbcae6d077b76bf7c33e42
Conny <jmg41490400@gmail.com>
2022-10-07 15:12:21 +0900

    #39 - 테스트 케이스 수정

--- src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
@@ -3,17 +3,18 @@ package com.fastcampus.projectboard.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
+import com.fastcampus.projectboard.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@Import(SecurityConfig.class)
+@DisplayName("View 루트 컨트롤러")
+@Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
@@ -23,6 +24,7 @@ class MainControllerTest {
         this.mvc = mvc;
     }
 
+    @DisplayName("[view][GET] 루트 페이지 -> 게시글 리스트 (게시판) 페이지 Redirection")
     @Test
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
         // Given
@@ -30,8 +32,7 @@ class MainControllerTest {
         // When & Then
         mvc.perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(MockMvcResultMatchers.view().name("forward:/articles"))
-            .andExpect(forwardedUrl("/articles"))
-            .andDo(MockMvcResultHandlers.print());
+            .andExpect(view().name("forward:/articles"))
+            .andExpect(forwardedUrl("/articles"));
     }
 }
