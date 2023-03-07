e7bf96a40a4f93f8f441fbed57692bffa2f08b2a
conny <jmg41490400@gmail.com>
2023-03-07 10:28:47 +0900

    #57 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -1,10 +1,14 @@
 package com.fastcampus.projectboard.dto.response;
 
+import com.fastcampus.projectboard.dto.ArticleCommentDto;
 import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.projectboard.dto.HashtagDto;
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -44,10 +48,30 @@ public record ArticleWithCommentsResponse(
             dto.userAccountDto().email(),
             nickname,
             dto.userAccountDto().userId(),
-            dto.articleCommentDtos().stream()
-                .map(ArticleCommentResponse::from)
-                .collect(Collectors.toCollection(LinkedHashSet::new))
+            organizeChildComments(dto.articleCommentDtos())
         );
+    }
+
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        // ID로 접근하기 위해 Map으로 변경
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                                                    .map(ArticleCommentResponse::from)
+                                                    .collect(
+                                                        Collectors.toMap(ArticleCommentResponse::id,
+                                                            Function.identity()));
+        map.values().stream()
+           .filter(ArticleCommentResponse::hasParentComment) // 자식 comment 가져오기
+           .forEach(comment -> {
+               ArticleCommentResponse parentComment = map.get(
+                   comment.parentCommentId()); // 부모 comment 가져오기
+               parentComment.childComments().add(comment); // 부모 댓글에 자식 댓글 저장
+           });
+        return map.values().stream()
+                  .filter(comment -> !comment.hasParentComment()) // 부모 댓글 가져오기
+                  .collect(Collectors.toCollection(() -> new TreeSet<>(Comparator
+                      .comparing(ArticleCommentResponse::createdAt)
+                      .reversed()
+                      .thenComparingLong(ArticleCommentResponse::id))));
     }
 
 }
