c3eaeccc53ab39ccc044c8e272a7f46ff04e33b5
conny <jmg41490400@gmail.com>
2023-03-07 18:17:35 +0900

    #68 - 회원 엔티티 디자인 수정: 인증이 없는 상태에서 회원 정보를 DB에 등록하기 위한 생성자 메소드 추가
    인증 정보가 없을 땐 직접 생성자, 수정자 정보를 입력하여
    회원 등록을 할 수 있도록 생성자 메소드를 추가함

--- src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
     @Index(columnList = "email", unique = true),
     @Index(columnList = "createdAt"),
@@ -42,17 +42,24 @@ public class UserAccount extends AuditingFields {
     }
 
     private UserAccount(String userId, String userPassword, String email, String nickname,
-        String memo) {
+        String memo, String createdBy) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
     }
 
     public static UserAccount of(String userId, String userPassword, String email, String nickname,
         String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return new UserAccount(userId, userPassword, email, nickname, memo, null);
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname,
+        String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
 
     @Override
