55172d29a3b8fed8257a90ac58d2fc6ef17fdf8f
conny <jmg41490400@gmail.com>
2023-03-07 18:19:53 +0900

    #68 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
@@ -3,8 +3,9 @@ package com.fastcampus.projectboard.config;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
-import com.fastcampus.projectboard.domain.UserAccount;
+import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.repository.UserAccountRepository;
+import com.fastcampus.projectboard.service.UserAccountService;
 import java.util.Optional;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -17,16 +18,27 @@ public class TestSecurityConfig {
     @MockBean
     private UserAccountRepository userAccountRepository;
 
-    // 테스트용 계정 생성
-    @BeforeTestMethod // 스프링테스트를 할때만 사용할 예정일때 사용
-    public void securitySetUp() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+    @MockBean
+    private UserAccountService userAccountService;
+
+    private static UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
             "unoTest",
             "pw",
             "uno-test@email.com",
             "uno-test",
             "test memo"
-        )));
+        );
     }
+
+    // 테스트용 계정 생성
+    @BeforeTestMethod // 스프링테스트를 할때만 사용할 예정일때 사용
+    public void securitySetUp() {
+        given(userAccountService.searchUser(anyString())).willReturn(
+            Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(),
+            anyString())).willReturn(createUserAccountDto());
+    }
+
 
 }
