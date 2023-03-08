314c4a97d6a1c10c4d8283cfebebd973972e3ad9
conny <jmg41490400@gmail.com>
2023-03-08 12:45:18 +0900

    #74 - Swagger UI 적용
    간단한 의존성 추가만으로 즉시 스웨거 문서화가 진행됨
    이 때 Spring Data Rest 가 자동으로 만들어 준 api를
    스웨거에 노출시키기 위해 추가 의존성을 사용함

--- src/main/java/com/fastcampus/projectboard/controller/MainController.java
+++ src/main/java/com/fastcampus/projectboard/controller/MainController.java
@@ -1,14 +1,39 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.dto.response.ArticleCommentResponse;
+import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * <p>
+ * 메인 컨트롤러
+ */
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String root() {
         return "forward:/articles";
+    }
+
+    /**
+     * <p>
+     * 댓글 정보를 열람한다.
+     *
+     * @param id 댓글 ID
+     * @return 댓글 응답
+     */
+    @ResponseBody
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(Long id) {
+        return ArticleCommentResponse.of(id,
+            "content",
+            LocalDateTime.now(),
+            "email",
+            "nickname",
+            "userId");
     }
 
 }
